4a2d7e200ff87b6d1f1ed5c0e0d0362b9398ea15
Ahmed Shaaban <ahmedlearningscience@gmail.com>
2025-07-18 18:14:02 +0300

    Enhancement

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSSL from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {port: 4444}
+  plugins: [react(), basicSSL()],
+  server: {port: 4444, https: true}
 })
